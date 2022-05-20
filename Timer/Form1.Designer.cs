00b717b3b8dc375fec537f6136117831e2bdf1d0
xrusteam <90495504+xrusteam@users.noreply.github.com>
2022-05-20 20:55:53 +0300

    Добавьте файлы проекта.

---
+++ Timer/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace WinTimer
+{
+    partial class WinTimer
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(547, 285);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(100, 41);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "Старт";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Location = new System.Drawing.Point(127, 285);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(100, 41);
+            this.cancelBtn.TabIndex = 1;
+            this.cancelBtn.Text = "Отмена";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(127, 107);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(100, 22);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(332, 107);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 4;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(547, 107);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown3.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(342, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 37);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Время";
+            // 
+            // WinTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 368);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.startBtn);
+            this.Name = "WinTimer";
+            this.Text = "Таймер";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Timer/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinTimer
+{
+    public partial class WinTimer : Form
+    {
+        public WinTimer()
+        {
+            InitializeComponent();
+            timer = new Timer();
+            timer.Tick += new EventHandler(TimeTick);
+            timer.Interval = 800;
+            
+        }
+        Timer timer = null;
+        private int StartCount = 0;
+        private string StringTimer(int time)
+        {
+            int hours = (time - (time % (60 * 60)) / (60 * 60));
+            int minutes = (time - time % 60) / 60 - hours * 60;
+            int seconds = time - hours * 60 * 60 - minutes * 60;
+            return String.Format("{0:00}:{1:00}:{2:00}", hours,minutes,seconds);
+        }
+        void TimeTick(object sender,EventArgs e)
+        {
+            if(StartCount != 0)
+            {
+                label1.Text = StringTimer(StartCount);
+                StartCount--;
+            }
+            else
+            {
+                (sender as Timer).Stop();
+                (sender as Timer).Dispose();
+            }
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            StartCount =  (int)numericUpDown1.Value +  (int)numericUpDown2.Value + (int)numericUpDown3.Value;
+            timer.Start();
+            
+        }
+    }
+}
